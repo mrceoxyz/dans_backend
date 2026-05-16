1eed8fb0070a9d36d242cf581b0dde59f006e602
mrceoxyz <mrceoxyz001@gmail.com>
2026-05-16 21:42:51 +0100

    url_update

--- tailor_shop_api/settings.py
+++ tailor_shop_api/settings.py
@@ -77,7 +77,8 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://127.0.0.1:3000",
-    "https://tailor-shop-ten.vercel.app"
+    "https://dans-beta.vercel.app/",
+
 ]
 
 CORS_ALLOW_CREDENTIALS = True
